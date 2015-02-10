a18d7efb05348bd812f49ed62ec0dfb00a9ef3b4
a.matsu <albel.gm@gmail.com>
2015-02-11 01:15:25 +0900

    initial commit

---
+++ Swift_de_TwitterTests/Swift_de_TwitterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_de_TwitterTests.swift
+//  Swift_de_TwitterTests
+//
+//  Created by 松浦 篤 on 2015/02/02.
+//  Copyright (c) 2015年 atsushi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Swift_de_TwitterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
